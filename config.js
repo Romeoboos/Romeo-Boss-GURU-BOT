0b9ee80fdafb6c2a9488f176963d4549dab1cc50
Romeoboos <116500466+Romeoboos@users.noreply.github.com>
2023-06-04 13:36:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94764070099', 'sandaru', true],
+  ['94764070099', 'Romeyo Boss', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'Romeyo Boss'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
